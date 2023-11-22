97838ca6d299198d34d3fc5f621b27ef111d9a91
ketni <ketni.tutachef@gmail.com>
2023-11-21 21:18:43 -0300

    commit main

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Tutachef',
+      title: 'Projeto Tutachef',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
